9fc983a72649b9e6b22beca0ed276ad98ef0f438
AsapNoRocky <98507187+AsapNoRocky@users.noreply.github.com>
2024-11-25 19:22:34 -0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# MySQL configuration
-class Config:
-    MYSQL_HOST = "localhost"
-    MYSQL_USER = "your_mysql_user"
-    MYSQL_PASSWORD = "your_mysql_password"
-    MYSQL_DB = "nba_betting_db"
-    MYSQL_CURSORCLASS = "DictCursor"
